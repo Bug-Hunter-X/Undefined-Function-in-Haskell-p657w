973d92a93d87644ed819bdd721840d6771658e77
BugHunter-X <askzenai@gmail.com>
2024-12-28 13:30:04 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,9 @@
+The solution corrects the typo in the function name, resulting in a correctly compiled and running program.
+
+```haskell
+undefinedFunction x = x + 1
+
+main = do
+  let result = undefinedFunction 5 
+  print result
+```
